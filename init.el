388353f97e8f2f7b86e35d2e12a301d7d7fb629d
annenpolka <lancelbb@gmail.com>
2024-02-28 09:32:37 +0900

    move some configs

--- init.el
+++ init.el
@@ -825,47 +825,6 @@
  (meow-setup)
  (meow-global-mode 1))
 
-
-;; better undo/redo
-(use-package undo-fu
-  :ensure t
-  :bind
-  ([remap undo] . undo-fu-only-undo)
-  ([remap redo] . undo-fu-only-redo))
-(use-package undo-fu-session
-  :ensure t
-  :config
-  (global-undo-fu-session-mode 1))
-
-;; better surround
-(use-package embrace)
-
-;; vim-like historical locate navigation
-(use-package backward-forward
-  :ensure t
-  :init
-  ;; reference: https://emacs-china.org/t/emacs/19171/17
-  (defun my/backward-forward-previous-location ()
-    "A `backward-forward-previous-location' wrap for skip invalid locations."
-    (interactive)
-    (let ((purge (< backward-forward-mark-ring-traversal-position (1- (length backward-forward-mark-ring))))
-          (recent (point-marker)))
-      (backward-forward-previous-location)
-      (when (and (equal recent (point-marker)) purge)
-        (my/backward-forward-previous-location))))
-
-  (defun my/backward-forward-next-location ()
-    "A `backward-forward-next-location' wrap for skip invalid locations."
-    (interactive)
-    (let ((purge (> backward-forward-mark-ring-traversal-position 0))
-          (recent (point-marker)))
-      (backward-forward-next-location)
-      (when (and (equal recent (point-marker)) purge)
-        (my/backward-forward-next-location))))
-  :config
-  (setq backward-forward-mark-ring-max 100)
-  (backward-forward-mode 1))
-
 ;; ==============================
 ;; Git
 ;; ==============================
