e29462a09a5f72d43c48613f6f2402b32dcc7514
annenpolka <lancelbb@gmail.com>
2024-02-28 09:58:04 +0900

    add embark

--- init.el
+++ init.el
@@ -193,26 +193,26 @@
       (split-window-below))
     (windmove-up))
 
- (defun move-or-create-window-below nil
-   "Move to the window below the current one, or create a new split if none exists."
-   (interactive)
-   (unless (window-in-direction 'below)
-     (split-window-vertically))
-   (windmove-down))
+  (defun move-or-create-window-below nil
+    "Move to the window below the current one, or create a new split if none exists."
+    (interactive)
+    (unless (window-in-direction 'below)
+      (split-window-vertically))
+    (windmove-down))
 
- (defun move-or-create-window-left nil
-   "Move to the window to the left of the current one, or create a new split if none exists."
-   (interactive)
-   (unless (window-in-direction 'left)
-     (split-window-right))
-   (windmove-left))
+  (defun move-or-create-window-left nil
+    "Move to the window to the left of the current one, or create a new split if none exists."
+    (interactive)
+    (unless (window-in-direction 'left)
+      (split-window-right))
+    (windmove-left))
 
-(defun move-or-create-window-right nil
-   "Move to the window to the right of the current one, or create a new split if none exists."
-   (interactive)
-   (unless (window-in-direction 'right)
-     (split-window-horizontally))
-   (windmove-right)))
+  (defun move-or-create-window-right nil
+    "Move to the window to the right of the current one, or create a new split if none exists."
+    (interactive)
+    (unless (window-in-direction 'right)
+      (split-window-horizontally))
+    (windmove-right)))
 
 (use-package savehist
   :elpaca nil
@@ -266,9 +266,9 @@
   ;; (setq mozc-candidate-style 'popup)
   ;; workaround for win
   (advice-add 'mozc-session-execute-command
-             :after (lambda (&rest args)
-                      (when (eq (nth 0 args) 'CreateSession)
-                        (mozc-session-sendkey '(Hankaku/Zenkaku))))))
+              :after (lambda (&rest args)
+                       (when (eq (nth 0 args) 'CreateSession)
+                         (mozc-session-sendkey '(Hankaku/Zenkaku))))))
 
 (use-package mozc-im
   :after mozc)
@@ -461,6 +461,37 @@
               ("M-A" . marginalia-cycle))
   :init
   (marginalia-mode))
+
+(use-package embark
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc. You may adjust the
+  ;; Eldoc strategy, if you want to see the documentation from
+  ;; multiple providers. Beware that using this can be a little
+  ;; jarring since the message shown in the minibuffer can be more
+  ;; than one line, causing the modeline to move up and down:
+
+  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
 
 ;; complition-styles
 (use-package orderless
